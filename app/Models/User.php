130e4192ea512ecacc28e120ec565f4becd4642b
yangying <suneyangying@gmail.com>
2021-04-28 10:15:55 +0900

    changewrong

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,5 +10,5 @@ class User extends Model
     use HasFactory;
 
     protected $table = 'users';
-    public $timestamps = false;//修改时报错：laravel每个表里默认有creat_at创建时间,和update_at更新时间
+    public $timestamps = false;//改增时报错：laravel每个表里默认有creat_at创建时间,和update_at更新时间
 }
