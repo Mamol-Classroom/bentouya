ef9d92e817a2591bf07cd2a4734037e48dee7154
huanganchi <sworn108@gmail.com>
2021-05-11 05:29:37 +0900

    update

---
+++ app/Models/bento.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bento extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bento';
+}
