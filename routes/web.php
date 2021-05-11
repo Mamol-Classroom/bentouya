5fef194340d1c735352a122da101524be6afaf82
huanganchi <sworn108@gmail.com>
2021-05-11 17:18:53 +0900

    'update'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
+use App\Http\Controllers\BentoController;
+use App\Http\Controllers\MypageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,23 @@ use App\Http\Controllers\TopController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', [TopController::class, 'top']);
+// ユーザー
 Route::get('/register', [TopController::class, 'register']);
 Route::post('/register-user', [TopController::class, 'registerUser']);
+Route::get('/register-success', [TopController::class, 'registerSuccess']);
+Route::get('/login', [TopController::class, 'login']);
+Route::post('/login', [TopController::class, 'login']);
+Route::get('/logout', [TopController::class, 'logout']);
+// 弁当
+Route::get('/bentos', [BentoController::class, 'index']);
+Route::get('/bento/add', [BentoController::class, 'add']);
+Route::post('/bento/add', [BentoController::class, 'add']);
+Route::get('/bento/add/complete', [BentoController::class, 'addComplete']);
+Route::post('/bento/delete', [BentoController::class, 'delete']);
+Route::get('/bento/update', [BentoController::class, 'update']);
+Route::post('/bento/update', [BentoController::class, 'update']);
+// マイページ
+Route::get('/mypage', [MypageController::class, 'index']);
+// 支払い
 
-Route::get('/user-list', [TopController::class, 'userList']);
-Route::get('/register-success',[TopController::class,'register_success']);
