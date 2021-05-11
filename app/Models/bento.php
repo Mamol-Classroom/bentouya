a6adf23927c7f2026902e91e5c3dc21a39ddb19d
huanganchi <sworn108@gmail.com>
2021-05-11 13:15:34 +0900

    'update'

--- app/Models/bento.php
+++ app/Models/bento.php
@@ -6,10 +6,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class bento extends Model
 {
     use HasFactory;
-
+    use softdeletes;
     protected $table = 'bento';
 }
