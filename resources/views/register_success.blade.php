39de5437162239ff4d57f81b1da00607b439e8bb
huanganchi <sworn108@gmail.com>
2021-05-11 16:49:03 +0900

    'update'

--- resources/views/register_success.blade.php
+++ resources/views/register_success.blade.php
@@ -3,20 +3,41 @@
 @section('title','註冊成功')
 
 @section('content')
-    <h1>登録した内容以下</h1>
-<main>
-    <table>
-        <p>メールアドレス:{{$email}}</p>
-        <p>名前:{{$name}}</p>
-        <p>郵便番号:{{$postcode}}</p>
-        <p>都道府県:{{$prefecture}}</p>
-        <p>市区町村:{{$city}}</p>
-        <p>住所:{{$address}}</p>
-        <p>電話番号:{{$tel}}</p>
-    </table>
-    <button type="button" onclick."location.href ='/'"><</button>
-</main>
 
-
-
+    <main class="center">
+        <h1>登録完了</h1>
+        <div>
+            <p>登録情報は下記になります</p>
+            <table class="register-table">
+                <tr>
+                    <td>メールアドレス</td>
+                    <td>{{ $email }}</td>
+                </tr>
+                <tr>
+                    <td>名前</td>
+                    <td>{{ $name }}</td>
+                </tr>
+                <tr>
+                    <td>郵便番号</td>
+                    <td>{{ $postcode }}</td>
+                </tr>
+                <tr>
+                    <td>都道府県</td>
+                    <td>{{ $prefecture }}</td>
+                </tr>
+                <tr>
+                    <td>市区町村</td>
+                    <td>{{ $city }}</td>
+                </tr>
+                <tr>
+                    <td>住所</td>
+                    <td>{{ $address }}</td>
+                </tr>
+                <tr>
+                    <td>電話番号</td>
+                    <td>{{ $tel }}</td>
+                </tr>
+            </table>
+        </div>
+    </main>
 @endsection
