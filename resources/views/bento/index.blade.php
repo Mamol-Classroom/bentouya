853c1fa1ac47918944ea193cb061af632654fce7
huanganchi <sworn108@gmail.com>
2021-05-11 16:34:26 +0900

    'update'

--- resources/views/bento/index.blade.php
+++ resources/views/bento/index.blade.php
@@ -1,1 +1,24 @@
-<?php
+@extends('layout')
+
+@section('title', '弁当管理')
+
+@section('content')
+    <main>
+        <h1 class="center">弁当管理</h1>
+        <div class="bento-container">
+            @foreach($bentos as $bento)
+                <div class="bento">
+                    <a href="/bento/update?bento_id={{ $bento->id }}">
+                        <p>{{ $bento->bento_name }}</p>
+                        <p>￥ {{ number_format($bento->price) }}</p>
+                    </a>
+                    <form method="post" action="/bento/delete">
+                        <input type="hidden" name="bento_id" value="{{ $bento->id }}">
+                        <button type="submit">販売終了</button>
+                    </form>
+                </div>
+            @endforeach
+        </div>
+    </main>
+@endsection
+
