9d0e88c32ce5f5df90d9efd38f152f084e87280d
huanganchi <sworn108@gmail.com>
2021-05-13 15:28:39 +0900

    'update'

---
+++ app/Models/favourite.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class favourite extends model
+{
+    use HasFactory;
+
+    protected $table = 'favourites';
+}
