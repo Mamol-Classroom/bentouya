0abad920454aa59b77f2f294240777310c3baf22
huanganchi <sworn108@gmail.com>
2021-05-14 15:35:51 +0900

    'update'

--- app/Models/Bento.php
+++ app/Models/Bento.php
@@ -8,9 +8,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
 
-class bento extends Model
+class Bento extends Model
 {
     use HasFactory;
     use softdeletes;
-    protected $table = 'bento';
+
+    protected $table = 'bentos';
+
+
+    public function is_favourite($user_id)
+    {
+        $bento_id = $this->id;
+        $favourite = Favourite::where('user_id', $user_id)
+            ->where('bento_id', $bento_id)
+            ->first();
+
+        return $favourite != null;
+    }
 }
