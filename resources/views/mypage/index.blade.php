ff83bdee418c1d0cbc85d10a849a83935b3b4e5f
huanganchi <sworn108@gmail.com>
2021-05-15 18:42:57 +0900

    'update'

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -6,7 +6,7 @@
         <div class="subview">
             <h1>プロフィール</h1>
             <div>
-                <form method="post" action="">
+                <form method="post" action="/mypage">
                     <table class="register-table">
                         <tr>
                             <td>メールアドレス</td>
