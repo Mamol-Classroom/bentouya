1e5a669550064ad893bf7e8c2e0631f1907693e4
Your Name <zeyu_lei@yahoo.com>
2021-05-18 00:53:08 +0900

    购物车前端美化

--- resources/views/order/cart.blade.php
+++ resources/views/order/cart.blade.php
@@ -4,35 +4,47 @@
 
 
 @section('content')
-<main id="main">
-    <div id="cart-item-controller">
-        <div class="bento-containter">
-            @if(count($bentos) == 0)
-                <a href="/">買い物を続ける</a>
-            @else
-                @foreach($bentos as $bento)
-                    <div class="bento">
-                        <a href="/bento/detail/{{$bento->id}}">
-                            <img src="{{ $bento->get_bento_image_url() }}" style="width: 180px; height:135px "/>
-                            <p>{{$bento->bento_name}}</p>
-                            <p>¥{{number_format($bento->price)}}</p>
-                            <p>数量：{{number_format($bento->quantity)}}</p>
-                        </a>
-                    </div>
-                @endforeach
-            @endif
-        </div>
+    <link rel="stylesheet" type="text/css" href="/css/cart.css">
+    <body>
+
+    @if(count($bentos) == 0)
+        <a href="/">買い物を続ける</a>
+    @else
+
+<table>
+    <thead>
+    <tr>
+        <th class="tdone">画像</th>
+        <th class="tdtwo">商品名</th>
+        <th class="tdthree">数量</th>
+        <th class="tdfour">単価</th>
+        <th class="tdfive">価格</th>
+        <th class="tdsix">操作</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    @foreach($bentos as $bento)
+    <tr class="trclass">
+        <td class="tdone"> <img src="{{ $bento->get_bento_image_url() }}" style="width: 180px; height:135px "/></td>
+        <td class="tdtwo ">{{$bento->bento_name}}</td>
+        <td class="tdthree"><span><input type="button" value="-"><span class="num">{{number_format($bento->quantity)}}</span><input type="button" value="+"></span></td>
+        <td class="tdfour"><span>単価：¥</span><span class="unit">{{number_format($bento->price)}}</span></td>
+        <td class="tdfive"><span>小計：</span><span class="subtal">0</span></td>
+        <td class="tdsix"><button>キャンセル</button></td>
+    </tr>
+    @endforeach
+    <tr><td   colspan="6"; class="talast"><span>商品件数：<span class="goods_num">0</span> 件; 合計金額： <span class="pricetal">{{number_format($total_price)}}</span> 円; </span></td></tr>
+    </tbody>
+</table>
+
+    @endif
+
+    <div >
+        <p>¥{{number_format($total_price)}}</p>
+        <button type="button">レジに進む</button>
     </div>
-        <div id="cart-btn-controller">
-            <p>¥{{number_format($total_price)}}</p>
-            <button type="button">レジに進む</button>
-        </div>
 
-
-
-
-</main>
-
-
+</body>
 
 @endsection
