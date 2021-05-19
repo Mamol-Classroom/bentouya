6e80fe52f4bf73c1c1c52d29f4ff3e380f7987a0
Wang Runbo <wangrunbo921@gmail.com>
2021-05-19 12:19:39 +0900

    注文

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Bento;
 use App\Models\Cart;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +13,185 @@ class OrderController extends Controller
 {
 
     public function index(Request $request)
+    {
+        $user = Auth::user();
+        $data = $request->session()->get('order.address');
+        $error_message = $request->session()->get('order.error_message');
+
+        if ($data == null) {
+            $data = [
+                'postcode' => $user->postcode,
+                'prefecture' => $user->prefecture,
+                'city' => $user->city,
+                'address' => $user->address,
+                'tel' => $user->tel,
+                'name' => $user->name,
+            ];
+        }
+
+        if ($error_message == null) {
+            $error_message = [
+                'postcode' => null,
+                'prefecture' => null,
+                'city' => null,
+                'address' => null,
+                'tel' => null,
+                'name' => null,
+            ];
+        }
+
+        $request->session()->forget('order.address');
+        $request->session()->forget('order.error_message');
+
+        return view('order.index', [
+            'data' => $data,
+            'error_message' => $error_message
+        ]);
+    }
+
+    public function order(Request $request)
+    {
+        $postcode = $request->post('postcode');
+        $prefecture = $request->post('prefecture');
+        $city = $request->post('city');
+        $address = $request->post('address');
+        $tel = $request->post('tel');
+        $name = $request->post('name');
+
+        $data = [
+            'postcode' => $postcode,
+            'prefecture' => $prefecture,
+            'city' => $city,
+            'address' => $address,
+            'tel' => $tel,
+            'name' => $name,
+        ];
+
+        $has_error = false;
+        $error_message = [
+            'postcode' => null,
+            'prefecture' => null,
+            'city' => null,
+            'address' => null,
+            'tel' => null,
+            'name' => null,
+        ];
+
+        if ($name == "") {
+            $error_message['name']  = '请输入姓名';
+            $has_error = true;
+        }
+
+        if ($postcode == "") {
+            $error_message['postcode']  = '请输入邮编';
+            $has_error = true;
+        }
+
+        if ($prefecture == "") {
+            $error_message['prefecture']  = '都道府県を入力してください';
+            $has_error = true;
+        }
+
+        if ($city == "") {
+            $error_message['city']  = '市区町村を入力してください';
+            $has_error = true;
+        }
+
+        if ($address == "") {
+            $error_message['address']  = '住所を入力してください';
+            $has_error = true;
+        }
+
+        if ($tel == "") {
+            $error_message['tel']  = '電話番号を入力してください';
+            $has_error = true;
+        }
+
+        if ($has_error) {
+            $request->session()->put('order.error_message', $error_message);
+            $request->session()->put('order.address', $data);
+
+            return redirect('/order');
+        }
+
+        $request->session()->put('order.address', $data);
+
+        return redirect('/payment');
+    }
+
+    public function payment(Request $request)
+    {
+        if ($request->method() === 'POST') {
+            // 处理支付
+            $card_no = $request->post('card_no');
+            $expire_month = $request->post('expire-month');
+            $expire_year = $request->post('expire-year');
+            $cvc = $request->post('cvc');
+            // 将上述数据传送给信用卡公司
+            // 等待支付处理完成，获得反馈（返回值 支付成功 / 支付失败）
+            // （假数据）
+            $payment_success = true;
+
+            // 生成订单数据
+            if (!$payment_success) {
+                // 支付失败
+                $request->session()->flash('payment.error_message', true);
+
+                return redirect('/payment');
+            }
+
+            // 支付成功，生成订单数据
+            $address = $request->session()->get('order.address');
+            $request->session()->forget('order.address');
+
+            $order = new Order();
+            $order->user_id = Auth::id();
+            $order->name = $address['name'];
+            $order->postcode = $address['postcode'];
+            $order->prefecture = $address['prefecture'];
+            $order->city = $address['city'];
+            $order->address = $address['address'];
+            $order->tel = $address['tel'];
+            $order->status = 1;
+            $order->save();
+
+            $cart_bentos = Cart::where('user_id', Auth::id())->get();
+            foreach ($cart_bentos as $cart) {
+                $bento = Bento::find($cart->bento_id);
+
+                $order_detail = new OrderDetail();
+                $order_detail->order_id = $order->id;
+                $order_detail->bento_id = $cart->bento_id;
+                $order_detail->bento_name = $bento->bento_name;
+                $order_detail->price = $bento->price;
+                $order_detail->bento_code = $bento->bento_code;
+                $order_detail->description = $bento->description;
+                $order_detail->guarantee_period = $bento->guarantee_period;
+                $order_detail->quantity = $cart->quantity;
+                $order_detail->save();
+
+                // 削减商品在库数
+                $bento->stock = $bento->stock - $order_detail->quantity;
+                $bento->save();
+
+                // 清空购物车
+                $cart->delete();
+            }
+
+            return redirect('/order/complete');
+        }
+
+        $payment_failed = $request->session()->has('payment.error_message');
+
+        return view('order.payment', ['payment_failed' => $payment_failed]);
+    }
+
+    public function complete(Request $request)
+    {
+        return view('order.complete');
+    }
+
+    public function cart(Request $request)
     {
         $carts = Cart::where('user_id', Auth::id())->get();
         $bento_list = [];
---
+++ resources/views/order/complete.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', '注文完了')
+
+@section('content')
+    <main class="center">
+        <h1>注文完了</h1>
+        <div>
+            <p>注文完了しました</p>
+        </div>
+    </main>
+@endsection
