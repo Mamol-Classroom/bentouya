0282c300c814796fb8b41d49d993dd7720d0f434
Your Name <zeyu_lei@yahoo.com>
2021-05-19 10:10:24 +0900

    nwe

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 
     <link rel="stylesheet" href="/css/fontawesome-free-5.15.3-web/css/all.css">
-
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
