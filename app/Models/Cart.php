77bba7a93ab285fbfc871bd3307b5cb13c9f4743
huanganchi <sworn108@gmail.com>
2021-05-19 13:32:54 +0900

    'model_rename'

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 
-class cart extends model
+class Cart extends model
 {
     use HasFactory;
 
